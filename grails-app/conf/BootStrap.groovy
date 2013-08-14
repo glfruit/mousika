11b4f55b149cee323a6148fc309402e6099f055c
glfruit <glfruit80@gmail.com>
2013-08-14 17:54:54 +0800

    调整初始化配置

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,4 @@
-import com.sanwn.mousika.Backup
-import com.sanwn.mousika.Course
-import com.sanwn.mousika.PrivilegeResource
-import com.sanwn.mousika.PrivilegeResourceMethod
-import com.sanwn.mousika.Role
-import com.sanwn.mousika.Teaching
-import com.sanwn.mousika.User
+import com.sanwn.mousika.*
 import org.apache.shiro.crypto.hash.Sha256Hash
 
 class BootStrap {
@@ -65,15 +59,15 @@ class BootStrap {
                     def teaching = new Teaching(course:course,capability: 10,frequency: 3,assignmentTimes: 2,checkTimes: 6,time: 100)
                     teaching.save(failOnError: true)*/
 
-                    def backup = new Backup(dataBasePath: "C:/Program Files/PostgreSQL/9.1/bin/pg_dump.exe",backupDelay: 0, autoBackupPeriod: 1.0)
+                    def backup = new Backup(dataBasePath: "C:/Program Files/PostgreSQL/9.1/bin/pg_dump.exe", backupDelay: 0, autoBackupPeriod: 1.0)
                     backup.save(failOnError: true)
 
                     def privilegeResource = new PrivilegeResource(controllerEn: "course", controllerCn: "课程")
-                    def privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "create", methodCn:"创建课程")
+                    def privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "create", methodCn: "创建课程")
                     privilegeResourceMethod1.save(failOnError: true)
-                    def privilegeResourceMethod2 = new PrivilegeResourceMethod(methodEn: "delete", methodCn:"删除课程")
+                    def privilegeResourceMethod2 = new PrivilegeResourceMethod(methodEn: "delete", methodCn: "删除课程")
                     privilegeResourceMethod2.save(failOnError: true)
-                    def privilegeResourceMethod3 = new PrivilegeResourceMethod(methodEn: "list", methodCn:"列表")
+                    def privilegeResourceMethod3 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
                     privilegeResourceMethod3.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod2)
@@ -81,64 +75,178 @@ class BootStrap {
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "user", controllerCn: "用户")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "create", methodCn:"创建用户")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "create", methodCn: "创建用户")
                     privilegeResourceMethod1.save(failOnError: true)
-                    privilegeResourceMethod2 = new PrivilegeResourceMethod(methodEn: "delete", methodCn:"删除用户")
+                    privilegeResourceMethod2 = new PrivilegeResourceMethod(methodEn: "delete", methodCn: "删除用户")
                     privilegeResourceMethod2.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod2)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "student", controllerCn: "学生")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn:"列表")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "fileResource", controllerCn: "文件资源")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn:"列表")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "backup", controllerCn: "备份")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn:"列表")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "assignment", controllerCn: "作业")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "save", methodCn:"保存")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "save", methodCn: "保存")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "auth", controllerCn: "认证")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "signIn", methodCn:"登录")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "signIn", methodCn: "登录")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "forum", controllerCn: "论坛")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "show", methodCn:"显示")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "show", methodCn: "显示")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "gradeBook", controllerCn: "成绩")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "show", methodCn:"显示")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "show", methodCn: "显示")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "resource", controllerCn: "资源")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "save", methodCn:"保存")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "save", methodCn: "保存")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
 
                     privilegeResource = new PrivilegeResource(controllerEn: "unitItem", controllerCn: "课程单元")
-                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "copy", methodCn:"复制")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "copy", methodCn: "复制")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+                }
+            }
+            production {
+
+                if (!User.count()) {
+                    def adminRole = new Role(name: "系统管理员")
+                    adminRole.addToPermissions("*:*")
+                    adminRole.save(failOnError: true)
+
+                    def teacherRole = new Role(name: "教师")
+                    teacherRole.addToPermissions("course:*")
+                    teacherRole.addToPermissions("assignment:*")
+                    teacherRole.addToPermissions("page:*")
+                    teacherRole.addToPermissions("feedback:*")
+                    teacherRole.addToPermissions("attempt:show")
+                    teacherRole.save(failOnError: true)
+
+                    def courseMgrRole = new Role(name: "课程负责人")
+                    courseMgrRole.addToPermissions("course:*")
+                    courseMgrRole.save(failOnError: true)
+
+                    def studentRole = new Role(name: "学生")
+                    studentRole.addToPermissions("student:*")
+                    studentRole.addToPermissions("course:index,list")
+                    studentRole.addToPermissions("assignment:show")
+                    studentRole.addToPermissions("attempt:create,update")
+                    studentRole.addToPermissions("feedback:show")
+                    studentRole.save(failOnError: true)
+
+                    /*def teachingAffairsRole = new Role(name: "教务处")
+                    teachingAffairsRole.addToPermissions("*:*")
+                    teachingAffairsRole.save(failOnError: true)*/
+
+                    def user = new User(username: "admin", fullname: "管理员", email: "admin@localhost.com", dateCreated: new Date(), passwordHash: new Sha256Hash("admin").toHex())
+                    user.addToRoles(adminRole)
+                    user.save(failOnError: true)
+
+                    def backup = new Backup(dataBasePath: "C:/Program Files/PostgreSQL/9.1/bin/pg_dump.exe", backupDelay: 0, autoBackupPeriod: 1.0)
+                    backup.save(failOnError: true)
+
+                    def privilegeResource = new PrivilegeResource(controllerEn: "course", controllerCn: "课程")
+                    def privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "create", methodCn: "创建课程")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    def privilegeResourceMethod2 = new PrivilegeResourceMethod(methodEn: "delete", methodCn: "删除课程")
+                    privilegeResourceMethod2.save(failOnError: true)
+                    def privilegeResourceMethod3 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
+                    privilegeResourceMethod3.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod2)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod3)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "user", controllerCn: "用户")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "create", methodCn: "创建用户")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResourceMethod2 = new PrivilegeResourceMethod(methodEn: "delete", methodCn: "删除用户")
+                    privilegeResourceMethod2.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod2)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "student", controllerCn: "学生")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "fileResource", controllerCn: "文件资源")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "backup", controllerCn: "备份")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "list", methodCn: "列表")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "assignment", controllerCn: "作业")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "save", methodCn: "保存")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "auth", controllerCn: "认证")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "signIn", methodCn: "登录")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "forum", controllerCn: "论坛")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "show", methodCn: "显示")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "gradeBook", controllerCn: "成绩")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "show", methodCn: "显示")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "resource", controllerCn: "资源")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "save", methodCn: "保存")
+                    privilegeResourceMethod1.save(failOnError: true)
+                    privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
+                    privilegeResource.save(failOnError: true)
+
+                    privilegeResource = new PrivilegeResource(controllerEn: "unitItem", controllerCn: "课程单元")
+                    privilegeResourceMethod1 = new PrivilegeResourceMethod(methodEn: "copy", methodCn: "复制")
                     privilegeResourceMethod1.save(failOnError: true)
                     privilegeResource.addToPrivilegeResourceMethods(privilegeResourceMethod1)
                     privilegeResource.save(failOnError: true)
--- grails-app/views/layouts/dojo.gsp
+++ grails-app/views/layouts/dojo.gsp
@@ -13,12 +13,13 @@
           href="${resource(dir: 'js/lib/dijit/themes/tundra', file: 'tundra.css')}"
           type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"
-          href="${resource(dir: 'css', file: 'bootstrap.css')}"
-          type="text/css"/>
-    <link rel="stylesheet"
-          href="${resource(dir: 'css', file: 'bootstrap-responsive.css')}"
-          type="text/css"/>
+    %{--<link rel="stylesheet"--}%
+    %{--href="${resource(dir: 'css', file: 'bootstrap.css')}"--}%
+    %{--type="text/css"/>--}%
+    %{--<link rel="stylesheet"--}%
+    %{--href="${resource(dir: 'css', file: 'bootstrap-responsive.css')}"--}%
+    %{--type="text/css"/>--}%
+    <r:require modules="bootstrap"/>
     <link rel="stylesheet"
           href="${resource(dir: 'css', file: 'mousika.css')}"
           type="text/css"/>
@@ -176,10 +177,12 @@
                             <i class="icon-user"></i>
                             <a href="${createLink(controller: 'user', action: 'list')}">用户管理</a>
                         </p>
+
                         <p>
                             <i class="icon-lock"></i>
                             <a href="${createLink(controller: 'privilege', action: 'list')}">权限管理</a>
                         </p>
+
                         <p>
                             <i class="icon-pencil"></i>
                             <a href="${createLink(controller: 'backup', action: 'list')}">系统备份</a>
